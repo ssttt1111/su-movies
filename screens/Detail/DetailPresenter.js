d104ae574ebc83dc86ec776346a0594b76e25224
Suwan Choi <ssttt1111@gmail.com>
2019-04-12 22:05:44 +0900

    #8.5 Finishing Up

--- screens/Detail/DetailPresenter.js
+++ screens/Detail/DetailPresenter.js
@@ -12,39 +12,34 @@ import Loader from "../../components/Loader";
 
 const Container = styled.ScrollView`
   background-color: ${BG_COLOR};
-  flex: 1;
 `;
 
 const Header = styled.View`
   position: relative;
-  justify-content: flex-end;
 `;
 
 const BgImage = styled.Image`
   width: ${Layout.width};
   height: ${Layout.height / 3.5};
-  opacity: 0.3;
   position: absolute;
   top: 0;
 `;
 
 const Content = styled.View`
-  flex: 1;
   flex-direction: row;
+  width: 80%;
   align-items: flex-end;
   padding-horizontal: 20px;
   height: ${Layout.height / 3.5};
-  width: ${Layout.width / 1.3};
 `;
 
-const Cloumn = styled.View`
+const Column = styled.View`
   margin-left: 30px;
 `;
 
 const Title = styled.Text`
-  /* width: 80%; */
   color: ${TINT_COLOR};
-  font-size: 16px;
+  font-size: 18px;
   font-weight: 600;
   margin-bottom: 10px;
 `;
@@ -60,21 +55,35 @@ const ContentTitle = styled.Text`
   margin-bottom: 10px;
 `;
 
-const Overview = styled.Text`
+const ContentValue = styled.Text`
   width: 80%;
   color: ${TINT_COLOR};
   font-size: 12px;
   margin-bottom: 10px;
 `;
 
+const DataContainer = styled.View`
+  margin-bottom: 10px;
+`;
+
+const Genres = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 12px;
+  margin-top: 10px;
+  width: 95%;
+`;
+
 const DetailPresenter = ({
-  id,
   posterPhoto,
   backgroundPhoto,
   title,
   voteAvg,
   overview,
-  loading
+  loading,
+  status,
+  date,
+  isMovie,
+  genres
 }) => (
   <Container>
     <Header>
@@ -91,10 +100,17 @@ const DetailPresenter = ({
       >
         <Content>
           <MoviePoster path={posterPhoto} />
-          <Cloumn>
+          <Column>
             <Title>{title}</Title>
             <MovieRating inSlide={true} votes={voteAvg} />
-          </Cloumn>
+            {genres ? (
+              <Genres>
+                {genres.map((genre, index) =>
+                  index === genres.length - 1 ? genre.name : `${genre.name} / `
+                )}
+              </Genres>
+            ) : null}
+          </Column>
         </Content>
       </LinearGradient>
     </Header>
@@ -102,7 +118,25 @@ const DetailPresenter = ({
       {overview ? (
         <>
           <ContentTitle>Overview</ContentTitle>
-          <Overview>{overview}</Overview>
+          <ContentValue>{overview}</ContentValue>
+        </>
+      ) : null}
+      {status ? (
+        <>
+          <DataContainer>
+            <ContentTitle>Status</ContentTitle>
+            <ContentValue>{status}</ContentValue>
+          </DataContainer>
+        </>
+      ) : null}
+      {date ? (
+        <>
+          <DataContainer>
+            <ContentTitle>
+              {isMovie ? "Realease Date" : "First Episode"}
+            </ContentTitle>
+            <ContentValue>{date}</ContentValue>
+          </DataContainer>
         </>
       ) : null}
       {loading ? <Loader /> : null}
@@ -117,7 +151,11 @@ DetailPresenter.propTypes = {
   title: PropTypes.string.isRequired,
   voteAvg: PropTypes.number,
   overview: PropTypes.string,
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
+  isMovie: PropTypes.bool.isRequired,
+  status: PropTypes.string,
+  date: PropTypes.string,
+  genres: PropTypes.array
 };
 
 export default DetailPresenter;
